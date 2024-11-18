d4aa0174566a82e2656cb5f1ad343d083c7a11a8
urandias <urandias@gmail.com>
2024-11-18 11:07:11 +0000

    started on basic layout of profile page

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,9 +1,15 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default function UserPage() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Profile</Text>
+            <Image style={styles.avatar} source={{ uri: 'https://cdn-icons-png.flaticon.com/512/149/149071.png' }}/>
+            <Text style={styles.text}>Username/Name</Text>
+            <Text style={styles.text}>Age:</Text>
+            <Text style={styles.text}>Weight:</Text>
+            <Text style={styles.text}>Height:</Text>
+            <TouchableOpacity style={styles.logout}>Log Out</TouchableOpacity>
+            <TouchableOpacity style={styles.editProfile}>Edit Profile</TouchableOpacity>
         </View>
     )
 }
@@ -15,7 +21,40 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    avatar: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 10
+    },
     text: {
         color: '#000',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    logout: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+        backgroundColor: '#D3D4D9',
+        color: '#000',
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderRadius: 5,
+        padding: 5,
+        borderColor: '#000',
+        borderWidth: 3
+    },
+    editProfile: {
+        position: 'absolute',
+        bottom: 100,
+        backgroundColor: '#D3D4D9',
+        color: '#000',
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderRadius: 5,
+        padding: 5,
+        borderColor: '#000',
+        borderWidth: 3
     }
 })
