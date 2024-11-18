fe16fab999aacfabb2eeed86d66d498a47573f1c
urandias <urandias@gmail.com>
2024-11-18 09:46:10 +0000

    added styling to nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,23 @@
 import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="user" options={{ title: "Profile" }} />
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: '#e91e63',
+      headerShown: false,
+      tabBarStyle: { backgroundColor: '#FFF9FB' }
+    }}>
+      <Tabs.Screen name="index" options={{ title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home' : 'home-outline'} size={24} color={color} />
+          )
+       }} />
+      <Tabs.Screen name="user" options={{ title: "Profile",
+        tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'person' : 'person-outline'} size={24} color={color} />
+        )
+       }} />
     </Tabs>
   )
 }
