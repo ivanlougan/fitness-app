2b3b8c8b29748047ef37fffd01fdb499b6b8661f
Elinavet <freeefauna@gmail.com>
2024-11-18 11:57:22 +0000

    add layout to the results

--- app/results.tsx
+++ app/results.tsx
@@ -1,9 +1,13 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function ResultPage() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Results</Text>
+            <Text style={styles.title}>Results:</Text>
+            <Text style={styles.text}>XP gained: 100XP</Text>
+            <Text style={styles.text}>Calories burnt: 100</Text>
+            <TouchableOpacity style={styles.shareButton}>Share results</TouchableOpacity>
+            <TouchableOpacity style={styles.button}>Finish</TouchableOpacity>
         </View>
     )
 }
@@ -16,6 +20,33 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     text: {
+        color: '#000',
+        fontSize: 20
+    },
+    title: {
+        color: '#000',
+        fontSize: 30,
+        marginBottom: 20
+    },
+    button:{
+        backgroundColor: '#D3D4D9',
+        marginTop: 20,
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 25,
+        color: '#000',
+        fontSize: 20
+    },
+    shareButton: {
+        marginTop: 20,
+        backgroundColor: '#4CAF50',
+        paddingVertical: 12,
+        paddingHorizontal: 25,
+        borderRadius: 25,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.25,
+        shadowRadius: 2,
         color: '#000',
     }
 })
