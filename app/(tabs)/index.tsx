995ed1f9024471681d5e0857b2673cd7d55feb74
Elinavet <freeefauna@gmail.com>
2024-11-20 10:32:17 +0000

    add Header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
 import { Text, View,  StyleSheet } from 'react-native';
 import { Link } from 'expo-router'
-import React, { useEffect } from 'react'
+import Header from '../Header';
 
 
 export default function HomePage() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
-      <Link href="/results" style={styles.text}>Go to Results</Link>
-      <Link href="/workout" style={styles.text}>Go to Workout</Link>
+      <Header title="FitnessApp" /> 
+
+      <View style={styles.content}>
+        <Text style={styles.text}>Home</Text>
+        <Link href="/results" style={styles.link}>Go to Results</Link>
+        <Link href="/workout" style={styles.link}>Go to Workout</Link>
+      </View>
     </View>
   );
 }
@@ -18,15 +22,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF9FB',
+    paddingTop: 50,
+  },
+  content: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     color: '#000',
+    fontSize: 20,
+    marginBottom: 10,
   },
   link: {
-    fontSize: 20,
+    fontSize: 18,
     color: '#000',
-    textDecorationLine: 'underline'
-  }
+    textDecorationLine: 'underline',
+    marginBottom: 10,
+  },
 });
---
+++ app/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    backgroundColor: '#666',
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+});
+
+export default Header;
