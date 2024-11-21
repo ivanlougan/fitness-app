dc7cf56230500ec0a006b5a7dda2a0cd837b90ae
urandias <urandias@gmail.com>
2024-11-21 09:12:52 +0000

    some stylling changes to the menu page

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,18 +1,18 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function MenuPage() {
     return (
         <View style={styles.container}>
-            <Link href="/bmi" style={styles.bmiButton} >
-                <Text style={styles.buttonText}>BMI</Text>
-            </Link>
-
-            <Link href="/calorie-tracker" style={styles.calButton}>
-                <Text style={styles.buttonText}>Calorie Tracker</Text>
-            </Link>
-
-            <Link href="/progess" style={styles.progressButton}>
+            <View style={styles.row}>
+                <Link href="/bmi" style={[styles.button, styles.bmiButton]}>
+                    <Text style={styles.buttonText}>BMI</Text>
+                </Link>
+                <Link href="/calorie-tracker" style={[styles.button, styles.calButton]}>
+                    <Text style={styles.buttonText}>Calorie Tracker</Text>
+                </Link>
+            </View>
+            <Link href="/progress" style={[styles.button, styles.progressButton]}>
                 <Text style={styles.buttonText}>Progress</Text>
             </Link>
         </View>
@@ -23,55 +23,39 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#FFF9FB',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20
+        padding: 20,
+        justifyContent: 'flex-start',
     },
-    text: {
-        color: '#000',
-        fontSize: 24,
-        marginBottom: 20
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 5, 
+    },
+    button: {
+        justifyContent: 'center', 
+        alignItems: 'center',  
+        borderRadius: 15,
     },
     bmiButton: {
         backgroundColor: '#4B88A2',
-        position: 'absolute',
-        top: 20,
-        left: 20,
-        width: '45%',
-        height: '25%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 25,
-        textAlign: 'center'
+        width: '48%',
+        aspectRatio: 1, 
     },
     calButton: {
         backgroundColor: '#4B88A2',
-        position: 'absolute',
-        top: 20,
-        right: 20,
-        width: '45%',
-        height: '25%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 25,
-        textAlign: 'center'
+        width: '48%',
+        aspectRatio: 1, 
     },
     progressButton: {
         backgroundColor: '#4B88A2',
-        top: -60,
-        left: 0,
-        right: 0,
         width: '100%',
-        height: '25%',
-        paddingVertical: 15,
-        paddingHorizontal: 40,
-        borderRadius: 25,
-        textAlign: 'center',
-        justifyContent: 'center',
+        height: '25%', 
+        marginTop: 10, 
     },
     buttonText: {
-        color: '#fff',
+        color: '#FFF',
         fontSize: 18,
         fontWeight: 'bold',
-    }
+        padding: 10
+    },
 });
