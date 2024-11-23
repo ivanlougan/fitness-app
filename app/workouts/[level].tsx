8c57c6e7bdc7f980f687ce6e6fb8470553aac67b
AlexMan123456 <alex.man03888@gmail.com>
2024-11-23 13:33:32 +0000

    Got the exercises to show for each workout

--- app/workouts/[level].tsx
+++ app/workouts/[level].tsx
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { getLevelExercises } from '../../api'
-import { useRouter } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 
 export default function Exercises() {
-
+const {level} = useLocalSearchParams()
+const [exercises, setExercises] = useState([])
+const [error, setError] = useState("")
+const [isLoading, setIsLoading] = useState(true)
+console.log(level)
+useEffect(() => {
+    setIsLoading(true)
+    getLevelExercises(level).then((exercises) => {
+        setIsLoading(false)
+        setExercises(exercises)
+    }).catch((err) => {
+        setIsLoading(false)
+        setError("Error fetching exercises")
+    })
+}, [])
 
     return (
         <View style={styles.container}>
-            <Text>Exercises</Text>
+            {exercises.map((exercise) => {
+                return (<div key={`${exercise._id}-container`}>
+                    <p key={`${exercise._id}-name`}>{exercise.name}</p>
+                    <p key={`${exercise._id}-description`}>{exercise.description}</p>
+                    <p key={`${exercise._id}-duration`}>{exercise.duration_in_seconds} seconds</p>
+                    <p key={`${exercise._id}-xp`}>+{exercise.xp} XP</p>
+                </div>)
+            })}
         </View>
     );
 }
