197b7a3815a5d3b112272926b610446987edcbe8
Elinavet <freeefauna@gmail.com>
2024-11-27 17:17:11 +0000

    change style in the menu

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,67 +1,66 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function MenuPage() {
     return (
-        <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
             <View style={styles.row}>
                 <Link href="/bmi" style={[styles.button, styles.bmiButton]}>
+                    <Ionicons name="body" size={60} color="white" style={styles.icon} />
                     <Text style={styles.buttonText}>BMI</Text>
                 </Link>
+            </View>
+            <View style={styles.row}>
                 <Link href="/calorie-tracker" style={[styles.button, styles.calButton]}>
+                    <Ionicons name="fast-food-outline" size={60} color="white" style={styles.icon} />
                     <Text style={styles.buttonText}>Calorie Tracker</Text>
                 </Link>
             </View>
-            <Link href="/progress" style={[styles.button, styles.progressButton]}>
-                <Text style={styles.buttonText}>Progress</Text>
-            </Link>
-        </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexGrow: 1,
         backgroundColor: '#FFF9FB',
         padding: 20,
-        justifyContent: 'flex-start',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 50,
+        paddingBottom: 50,
     },
     row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 5,
+        width: '160%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20,
     },
     button: {
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 15,
+        flexDirection: 'row',
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        width: '50%',
+        aspectRatio: 1,
+        marginTop: 20,
     },
     bmiButton: {
         backgroundColor: '#4B88A2',
-        width: '48%',
-        aspectRatio: 1,
     },
     calButton: {
         backgroundColor: '#4B88A2',
-        width: '48%',
-        aspectRatio: 1,
     },
-    cookButton: {
-        backgroundColor: '#4B88A2',
-        width: '100%',
-        height: '25%', 
-        marginTop: 10,
-    },
-    progressButton: {
-        backgroundColor: '#4B88A2',
-        width: '100%',
-        height: '25%',
-        marginTop: 20, 
+    icon: {
+        marginRight: 10,
     },
     buttonText: {
         color: '#FFF',
-        fontSize: 18,
+        fontSize: 26,
         fontWeight: 'bold',
-        padding: 10,
+        textAlign: 'center',
     },
 });
